d61c99895a3d535b9888c0837982d44d64c1dbfc
rishan-noetic <rishan.antonio@wearenoetic.com>
2020-03-17 16:16:23 +0530

    meesages

--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -3,9 +3,20 @@
 const express = require('express')
 const router = express.Router()
 const Feed = require('../../models/feed.model')
+const Message = require('../../models/feed.model')
 
 router.get('/getFeed', (req, res) => {
     Feed.find({})
+        .then((data) => {
+            res.send(data)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+}) // api status
+
+router.get('/getMessages', (req, res) => {
+    Message.find({})
         .then((data) => {
             res.send(data)
         })
